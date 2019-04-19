c595d5f891f96404f919fdf8da8fb3681fc83f16
ecshaff <emmyshaff@gmail.com>
2019-04-18 22:25:04 -0700

    updated playlist.html

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 
 // http://api.openweathermap.org/data/2.5/group?id=4440906&appid=7281cf70115d4883b2168624646518dd&units=metric
 
-$("#submitButton").click(function (e) {
+// $("#submitButton").click(function (e) {
 
 $.ajax({
 type: "POST",
-url: "https://api.openweathermap.org/data/2.5/group?id=4440906,3120501,3128760,5128581,4930956&appid=7281cf70115d4883b2168624646518dd&units=metric",
+url: "https://api.openweathermap.org/data/2.5/group?id=5809844,5391811,5128581,4164138,4671654&appid=7281cf70115d4883b2168624646518dd&units=metric",
 dataType: "json",
 
 success: function (result, status, xhr) {
@@ -27,7 +27,7 @@ error: function (xhr, status, error) {
 console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
 }
 });
-});
+// });
 function CreateWeatherJson(json) {
 var newJson = "";
 for (i = 0; i < json.list.length; i++) {
--- playlist.js
+++ playlist.js
@@ -18,7 +18,7 @@ $closeBtn.on('click', close);
 
 const $openBtn1 = $('#generate1');
 const $modal1 = $('#modal1');
-const $closeBtn1 = $('#close1');
+const $closeBtn1 = $('.close');
 
 
 const generate1 = () => {
@@ -35,7 +35,7 @@ $closeBtn1.on('click', close1);
 
 const $openBtn2 = $('#generate2');
 const $modal2 = $('#modal2');
-const $closeBtn2 = $('#close2');
+const $closeBtn2 = $('.close');
 
 
 const generate2 = () => {
@@ -49,6 +49,96 @@ const close2 = () => {
 $openBtn2.on('click', generate2);
 
 $closeBtn2.on('click', close2);
+
+
+const $openBtn3 = $('#generate3');
+const $modal3 = $('#modal3');
+const $closeBtn3 = $('.close');
+
+
+const generate3 = () => {
+	$modal3.show()
+}
+const close3 = () => {
+	$modal3.hide(500)
+}
+
+
+$openBtn3.on('click', generate3);
+
+$closeBtn3.on('click', close3);
+
+
+const $openBtn4 = $('#generate4');
+const $modal4 = $('#modal4');
+const $closeBtn4 = $('.close');
+
+
+const generate4 = () => {
+	$modal4.show()
+}
+const close4 = () => {
+	$modal4.hide(500)
+}
+
+
+$openBtn4.on('click', generate4);
+
+$closeBtn4.on('click', close4);
+
+
+const $openBtn5 = $('#generate5');
+const $modal5 = $('#modal5');
+const $closeBtn5 = $('.close');
+
+
+const generate5 = () => {
+	$modal5.show()
+}
+const close5 = () => {
+	$modal5.hide(500)
+}
+
+
+$openBtn5.on('click', generate5);
+
+$closeBtn5.on('click', close5);
+
+
+const $openBtn6 = $('#generate6');
+const $modal6 = $('#modal6');
+const $closeBtn6 = $('.close');
+
+
+const generate6 = () => {
+	$modal6.show()
+}
+const close6 = () => {
+	$modal6.hide(500)
+}
+
+
+$openBtn6.on('click', generate6);
+
+$closeBtn6.on('click', close6);
+
+
+const $openBtn7 = $('#generate7');
+const $modal7 = $('#modal7');
+const $closeBtn7 = $('.close');
+
+
+const generate7 = () => {
+	$modal2.show()
+}
+const close7 = () => {
+	$modal7.hide(500)
+}
+
+
+$openBtn7.on('click', generate7);
+
+$closeBtn7.on('click', close7);
 
 
 
